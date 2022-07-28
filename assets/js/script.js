59259eb9de3fe40a206eb20a1cad4f814c02780b
ParksFG <ParkerFGay@gmail.com>
2022-07-27 21:37:10 -0500

    HTML tidying, JS added formatting for time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 // Variable for moment day
 var currentDay = moment().format("dddd, MMMM Do YYYY")
+var currentTime = moment().hour()
 
 // Setting the day and time on the webpage
 $("#currentDay").text("Today is: " + currentDay)
+
+// Formatting on text areas depending on time change the currentTime variable to test
+for(var i = 8; i < 18; i++) {
+    if(i < currentTime) {
+        $("#" + i).addClass("past");
+    } else if(i == currentTime) {
+        $("#" + i).addClass("present");
+    } else if(i > currentTime) {
+        $("#" + i).addClass("future");
+    }
+}
